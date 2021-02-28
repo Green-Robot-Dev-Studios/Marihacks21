5260cb1bfe1161c4a663882a64ef8f476bc779f2
marcushobbies <60950803+marcushobbies@users.noreply.github.com>
2021-02-27 22:29:39 -0500

    Update

--- dashboard.js
+++ dashboard.js
@@ -1,13 +1,29 @@
+
 async function entry(){
   var codeText = document.getElementById('classcode');
   codeText.innerHTML = "Class Code: " + await getClassroomCode(uid);
 
+  var qList = document.getElementById('qList');
+
   //Listing all questions (BROKEN)
-  var qs = await getClassroom(uid)[0][4];
-  console.log(qs);
+  var classroom = await getClassroom(uid);
+  //console.log(qs[0]["questions"]);
+  var qs = classroom[0]["questions"];
+  console.log(classroom);
+
+  var qListHtml = "";
 
   for(var num = 0; num < qs.length; num++){
-      console.log(qs[num]);
+      //console.log(qs[num]);
+      qListHtml = qListHtml + `
+                  <form class="question" action="addQuestion.html" method="get">
+                      <input type="text" name="question" value="` + qs[num]["question"] + `" readonly>
+                      <input type="submit" name="submit" value="Edit">
+                      <input type="submit" name="submit" value="Send Question" formaction="sendQuestion.html">
+                  </form>
+      `;
   }
+
+  qList.innerHTML = qListHtml;
 
 }

6fe82c53d285d6bbcd446294425a0bb1a7a4bcd4
marcushobbies <60950803+marcushobbies@users.noreply.github.com>
2021-02-27 21:33:19 -0500

    Started questions

--- dashboard.js
+++ dashboard.js
@@ -2,8 +2,12 @@ async function entry(){
   var codeText = document.getElementById('classcode');
   codeText.innerHTML = "Class Code: " + await getClassroomCode(uid);
 
+  //Listing all questions (BROKEN)
+  var qs = await getClassroom(uid)[0][4];
+  console.log(qs);
 
-
-
+  for(var num = 0; num < qs.length; num++){
+      console.log(qs[num]);
+  }
 
 }
